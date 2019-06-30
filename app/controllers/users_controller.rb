3f9381325848ff94773fd3407ed2ad40453fc3b4
Diego Selzlein <diegoselzlein@gmail.com>
2019-06-30 17:53:36 -0300

    Page to list users and lock/unlock them.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[lock unlock]
+  before_action :authenticate_user!
+
+  def index
+    @users = User.order(:email).page(params[:page])
+  end
+
+  def lock
+    @user.lock_access!
+    respond_to do |format|
+      format.html { redirect_to users_path, notice: 'User was locked' }
+      format.json { head :no_content }
+    end
+  end
+
+  def unlock
+    @user.unlock_access!
+    respond_to do |format|
+      format.html { redirect_to users_path, notice: 'User was unlocked' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :devices, only: %i[create index show]
   resources :device_snapshots, only: :create
   resources :issues, only: %i[index show destroy]
+  resources :users, only: :index do
+    put :lock, on: :member
+    put :unlock, on: :member
+  end
 
   devise_for :users
 end
